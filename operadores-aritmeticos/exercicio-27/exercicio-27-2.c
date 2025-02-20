d2f65af5ace376c4c7179c5f5b6614197cdd22ab
brunaportelaa <101227338+brunaportelaa@users.noreply.github.com>
2025-02-20 10:40:47 -0300

    ajuste exercício 27 v2

--- operadores-aritmeticos/exercicio-27/exercicio-27-2.c
+++ operadores-aritmeticos/exercicio-27/exercicio-27-2.c
@@ -9,18 +9,12 @@
 int main() {
 
     int codigo;
-    int varS;
     int digitoV;
-
-    printf("-----CÓDIGO VERIFICADOR DÍGITO V------\n");
 
     printf("Forneça um código de 5 algarismos: \n");
     scanf("%d", &codigo);
-
-    varS = (6 * ((codigo / 10000) % 10)) + (5 * ((codigo / 1000) % 10)) + (4 * ((codigo / 100) % 10)) + (3 * ((codigo / 10) % 10)) + (2 * (codigo % 10));
-    printf("Valor de S: %d\n", varS);
-
-    digitoV = varS % 7;
+    
+    digitoV = (6 * ((codigo / 10000) % 10)) + (5 * ((codigo / 1000) % 10)) + (4 * ((codigo / 100) % 10)) + (3 * ((codigo / 10) % 10)) + (2 * (codigo % 10)) % 7;
     printf("Dígito V: %d\n", digitoV);
 
 }
