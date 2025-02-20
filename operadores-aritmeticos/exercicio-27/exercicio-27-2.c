9d108ba56e38fa4cea17f66ad42dcc7e4704f8ef
brunaportelaa <101227338+brunaportelaa@users.noreply.github.com>
2025-02-20 10:48:40 -0300

    exercício 27 - v2

--- operadores-aritmeticos/exercicio-27/exercicio-27-2.c
+++ operadores-aritmeticos/exercicio-27/exercicio-27-2.c
@@ -9,12 +9,9 @@
 int main() {
 
     int codigo;
-    int digitoV;
-
     printf("Forneça um código de 5 algarismos: \n");
     scanf("%d", &codigo);
-    
-    digitoV = (6 * ((codigo / 10000) % 10)) + (5 * ((codigo / 1000) % 10)) + (4 * ((codigo / 100) % 10)) + (3 * ((codigo / 10) % 10)) + (2 * (codigo % 10)) % 7;
-    printf("Dígito V: %d\n", digitoV);
+    codigo = ((6 * ((codigo / 10000) % 10)) + (5 * ((codigo / 1000) % 10)) + (4 * ((codigo / 100) % 10)) + (3 * ((codigo / 10) % 10)) + (2 * (codigo % 10))) % 7;
+    printf("Dígito V: %d\n", codigo);
 
 }
