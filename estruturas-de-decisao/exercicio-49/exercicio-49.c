4db7ee6ad8ef8703514bddcc24828e4ef9486754
brunaportelaa <101227338+brunaportelaa@users.noreply.github.com>
2025-02-22 21:15:30 -0300

    exercícios 48 e 49

--- estruturas-de-decisao/exercicio-49/exercicio-49.c
+++ estruturas-de-decisao/exercicio-49/exercicio-49.c
@@ -1,25 +1,37 @@
 // SÍNTESE
-//     ENTRADA: 
-//     PROCESSAMENTO: 
-//     SAÍDA: 
+//     ENTRADA: valorX, valorY e valorZ (int)
+//     PROCESSAMENTO: verifica se é possível formar um triângulo e, se sim, classifica o triângulo
+//     SAÍDA: printa se o triângulo pode ou não ser formado e, se sim, classifica o triângulo
 
 #include <stdio.h>
 
 int main() {
 
-    int var;
+    int valorX;
+    int valorY;
+    int valorZ;
     
-    printf("-----TITULO-------\n");
+    printf("Insira o valor X:\n");
+    scanf("%d", &valorX);
 
-    printf("Direcinamento: \n");
-    scanf("%d", &var);
+    printf("Insira o valor Y:\n");
+    scanf("%d", &valorY);
 
-    if() {
+    printf("Insira o valor X:\n");
+    scanf("%d", &valorZ);
 
+    if (valorX < valorY + valorZ && valorY < valorX + valorZ && valorZ < valorX + valorY) {
+        printf("Triângulo formado!\n");
+    } 
+    
+    if ( valorX == valorY && valorY == valorZ) {
+        printf("Esse triângulo é EQUILÁTERO.\n");
+    } else if ( valorX == valorY || valorY == valorZ || valorX == valorZ ) {
+        printf("Esse triângulo é ISÓSCELE.\n");
+    } else if ( valorX != valorY || valorY != valorZ || valorX != valorZ ) {
+        printf("Esse triângulo é ESCALENO.\n");
     } else {
-        
+        printf("Infelizmente, não é possível formar um retângulo com esses valores :/");
     }
-
-    printf("Retorno final: %d\n", var);
 
 }
