0ab1a71472479c1d10458af1e38e3cc5135763c8
Bruna Portela <brunaporteladj@gmail.com>
2025-03-03 18:41:35 -0300

    Criados os arquivos 136-150 e 72-87

--- repeticao-do-while/exercicio-125/exercicio-125.c
+++ repeticao-do-while/exercicio-125/exercicio-125.c
@@ -7,5 +7,20 @@
 
 int main() {
 
+    int i;
+    int elevador;
+    int andares;
+    int variacao;
+
+    printf("Informe quantos andares tem o prédio: \n");
+    scanf("%d", &andares);
+
+    for ( i = 1; i <= andares; i++)
+    {
+        printf("Estamos no %do andar\n");
+        printf("Informe quantas pessoas entraram (número positivo) ou saíram (número negativo) do elevador: \n");
+        scanf("%d", &variacao);
+    }
+    
 
 }
