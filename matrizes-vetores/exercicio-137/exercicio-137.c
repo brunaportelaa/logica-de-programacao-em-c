15284191d7830ecc09dec355810011e7cd70c81f
Bruna Portela <brunaporteladj@gmail.com>
2025-03-03 23:44:22 -0300

    Exercícios 136 a 138

--- matrizes-vetores/exercicio-137/exercicio-137.c
+++ matrizes-vetores/exercicio-137/exercicio-137.c
@@ -7,5 +7,21 @@
 
 int main() {
 
+    int i;
+    int vetor1[5] = { 1, 2, 3, 4, 5};
+    int vetor2[5] = { 5, 6, 7, 8, 9};
+    int vetor3[5];
+
+    for ( i = 0; i < 5; i++)
+    {
+        vetor3[i] = vetor1[i] + vetor2[i];
+    }
+
+    printf("Conteúdo do vetor 2:\n");
+
+    for ( i = 0; i < 5; i++)
+    {
+        printf("%d, ", vetor3[i]);
+    }
 
 }
--- matrizes-vetores/exercicio-138/exercicio-138.c
+++ matrizes-vetores/exercicio-138/exercicio-138.c
@@ -7,5 +7,29 @@
 
 int main() {
 
+    int i = 0;
+    int j = 5;
+    int vetor1[5] = { 1, 2, 3, 4, 5};
+    int vetor2[5] = { 5, 6, 7, 8, 9};
+    int vetor3[10];
+
+    for ( i = 0; i < 5; i++)
+    {
+        vetor3[i] = vetor1[i];
+    }
+
+    printf("Conteúdo do vetor 2:\n");
+
+    for ( i = 0; i < 5; i++)
+    {
+        vetor3[j] = vetor2[i];
+        j++;
+    }
+    
+    for ( i = 0; i < 10; i++)
+    {
+        printf("%d, ", vetor3[i]);
+    }
+    
 
 }
